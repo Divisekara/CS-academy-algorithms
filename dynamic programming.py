8bc371c10f1a02be722a48874369d272c4e2dc98
Divisekara <9asitha7@gmail.com>
2020-10-19 16:55:45 +0530

    2

--- dynamic programming.py
+++ dynamic programming.py
@@ -2,4 +2,18 @@ def fib_bottom_up(n):
     """
     fibbonacci function using bottom up approach
     """
-    if()
+    if(n == 1 or n==2 ):
+        return 1
+    
+    bottom_up = [None] * (n+1)
+    bottom_up[1] = 1
+    bottom_up[2] = 1
+
+    for i in range(3, n+1):
+        bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
+    
+    return bottom_up[n]
+
+
+print(fib_bottom_up(10000))
+    
