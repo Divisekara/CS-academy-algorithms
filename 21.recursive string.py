286f61c8bac0c0d23b372df43c01b1467fbd18ce
Divisekara <9asitha7@gmail.com>
2020-10-20 05:13:04 +0530

    1

--- 21.recursive string.py
+++ 21.recursive string.py
@@ -1,24 +1,23 @@
 n, k =list(map(int, input().strip().split(" ")))
 
-def f(n):
-    if(n == 0):
-        return "a"
-    if(n == 1):
-        return 'b'
-    if(n == 2):
-        return 'c'
 
+if(n == 0 and k==1):
+    print ("a")
+elif(n == 1 and k==1):
+    print ('b')
+elif(n == 2 and k==1):
+    print ('c')
+
+
+else:
     a = 'a'
     b = 'b'
     c = 'c'
 
     for i in range(2,n):
-        a,b,c = b,c, a+b+c
-    
-    return c
+        a,b,c = b,c, c+b+a
 
-word = f(n) 
-if(len(word) < k ):
-    print(-1)
-else:
-    print(word[k-1])
+    if(len(c) < k ):
+        print(-1)
+    else:
+        print(c[k-1])
