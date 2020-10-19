72bec99e367f6b27e5aa6a1720b50a53c618d5a2
Divisekara <9asitha7@gmail.com>
2020-10-20 03:35:48 +0530

    3

--- 20.max even subarray.py
+++ 20.max even subarray.py
@@ -1,21 +1,27 @@
-n = int(input().strip())
-L = list(map(int, input().strip().split(" ")))
+# n = int(input().strip())
+# L = list(map(int, input().strip().split(" ")))
+ 
+# current_sum = -(10**10) #sum(list(map(abs, L))) * (-1)
 
-M = [0] * (n) 
+# for i in range(1,n):
 
-for i in range(1,n):
+#     for j in range((i+1)%2, i, 2):
+#         total = sum(L[j:i+1])
 
-    current_sum = sum(list(map(abs, L))) * (-1)
+#         current_sum = max(current_sum, total)
 
-    for j in range((i+1)%2, i, 2):
-        total = sum(L[j:i+1])
-
-        if(current_sum < total):
-            current_sum = total
-    
-    M[i] = current_sum
-
-M.pop(0)
-print(max(M))
+# print(current_sum)
 
 
+n = int(input())
+a = list(map(int,input().split(' ')))
+
+dp = [0 for i in range(n+2)]
+
+mx = -2 * 10 ** 9
+for i in range(n-2,-1,-1):
+    dp[i] = a[i] + a[i+1] + dp[i+2]
+    mx = max(mx, dp[i])
+    dp[i] = max(0,dp[i])
+
+print(mx)
--- sample.py
+++ sample.py
@@ -1,2 +1,3 @@
-
+for i in range(10, -5, -1):
+    print(i)
 
